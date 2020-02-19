1acf9301e1aec8ec9ef51374cf91248d32e7e77b
Wataru.Kasahara <kasahara1019@gmail.com>
2020-02-19 23:14:30 +0900

    yarn ng g c features/mobtimer/ui/presenters/members

--- src/app/features/mobtimer/mobtimer.module.ts
+++ src/app/features/mobtimer/mobtimer.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MobtimerComponent } from './ui/pages/mobtimer/mobtimer.component';
+import { MembersComponent } from './ui/presenters/members/members.component';
 import { TimerComponent } from './ui/presenters/timer/timer.component';
 
 @NgModule({
-  declarations: [MobtimerComponent, TimerComponent],
+  declarations: [MobtimerComponent, TimerComponent, MembersComponent],
   imports: [CommonModule, ReactiveFormsModule],
 })
 export class MobtimerModule {}
---
+++ src/app/features/mobtimer/ui/presenters/members/members.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MembersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
