fd441592ba1b099684585d9da8b9e701fe8ab004
Wataru.Kasahara <kasahara1019@gmail.com>
2020-02-19 21:37:20 +0900

    アプリケーション名を変更

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,6 +20,6 @@ describe('AppComponent', () => {
   it(`should have as title 'ng-basis'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-basis');
+    expect(app.title).toEqual('MobTimer');
   });
 });
