c9fb4fe6f99309f91f05ed6433626e6ef3a34d78
Wataru.Kasahara <kasahara1019@gmail.com>
2020-02-20 10:02:53 +0900

    StoreModule と StoreDevtoolsModule をインポート

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MobtimerModule } from './features/mobtimer/mobtimer.module';
@@ -8,7 +10,15 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule, SharedModule, MobtimerModule],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
+    AppRoutingModule,
+    SharedModule,
+    MobtimerModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
