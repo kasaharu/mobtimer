a57190b1b2a83071009ad0257febb97d0ca08cde
Wataru.Kasahara <kasahara1019@gmail.com>
2020-02-22 22:14:37 +0900

    MobTimerStore を実装

--- src/app/features/mobtimer/store/index.store.ts
+++ src/app/features/mobtimer/store/index.store.ts
@@ -1,26 +1,35 @@
 import { createAction, createReducer, on, props, union } from '@ngrx/store';
+import { createFeatureStoreSelector } from '../../../shared/store/helpers/selector';
 
 // NOTE: State
 export interface State {
-  index: any | null;
+  mobTime: number;
+  mobMembers: string[];
 }
 
 export const initialState: State = {
-  index: null,
+  mobTime: 0,
+  mobMembers: [],
 };
 
 // NOTE: Actions
-export const saveIndex = createAction('[Index] save', props<{ index: any }>());
+export const saveMobTime = createAction('[MobTimer] save mobTime', props<{ mobTime: number }>());
+export const saveMobMember = createAction('[MobTimer] save mobMember', props<{ mobMember: string }>());
 
-export const actions = { saveIndex };
+export const actions = { saveMobTime, saveMobMember };
 const actionsUnion = union(actions);
 
 // NOTE: Reducer
-const indexReducer = createReducer(initialState, on(saveIndex, (state, { index }) => ({ ...state, index })));
+const mobTimerReducer = createReducer(
+  initialState,
+  on(saveMobTime, (state, { mobTime }) => ({ ...state, mobTime })),
+  on(saveMobMember, (state, { mobMember }) => ({ ...state, mobMembers: [...state.mobMembers, mobMember] })),
+);
 
 export default function reducer(state: State, action: typeof actionsUnion): State {
-  return indexReducer(state, action);
+  return mobTimerReducer(state, action);
 }
 
 // NOTE: Selectors
-export const featureName = 'index';
+export const featureName = 'mobTimer';
+export const selectStore = createFeatureStoreSelector<State>(featureName);
