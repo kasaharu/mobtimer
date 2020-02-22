86f4cd700061cdb1f546c20323cd6baa9d4cda56
Wataru.Kasahara <kasahara1019@gmail.com>
2020-02-22 22:27:09 +0900

    MobtimerStore を featureStore としてインポート

--- src/app/features/mobtimer/mobtimer.module.ts
+++ src/app/features/mobtimer/mobtimer.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { default as reducer, featureName } from './store/index.store';
 import { MobtimerComponent } from './ui/pages/mobtimer/mobtimer.component';
 import { MembersComponent } from './ui/presenters/members/members.component';
 import { TimerComponent } from './ui/presenters/timer/timer.component';
 
 @NgModule({
   declarations: [MobtimerComponent, TimerComponent, MembersComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, StoreModule.forFeature(featureName, reducer)],
 })
 export class MobtimerModule {}
