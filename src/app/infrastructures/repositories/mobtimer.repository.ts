2a1359d9ec89fa7b093b4d7a7475b6da96e47413
Wataru.Kasahara <kasahara1019@gmail.com>
2020-02-25 09:48:59 +0900

    time の保存と取得のメソッドを実装

--- src/app/infrastructures/repositories/mobtimer.repository.ts
+++ src/app/infrastructures/repositories/mobtimer.repository.ts
@@ -5,4 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class MobtimerRepository {
   constructor() {}
+  readonly storagePrefix = 'kasaharu/mobtimer';
+
+  saveTime(minutes: number) {
+    localStorage.setItem(`${this.storagePrefix}/minutes`, minutes.toString());
+  }
+
+  getTime(): number {
+    const minutes = localStorage.getItem(`${this.storagePrefix}/minutes`);
+    return minutes ? Number(minutes) : 0;
+  }
 }
