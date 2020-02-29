47177aa0bbb307d6d48ee640c51e1b694ac0b1e8
Wataru.Kasahara <kasahara1019@gmail.com>
2020-02-29 19:43:55 +0900

    作成しようとしている MobMember の存在確認をする処理をドメインサービスに実装

--- src/app/domain/mobbing/mob-member.service.ts
+++ src/app/domain/mobbing/mob-member.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { MobtimerRepository } from '../../infrastructures/repositories/mobtimer.repository';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MobMemberService {
+  constructor() {}
 
-  constructor() { }
+  static exists(name: string) {
+    const repo = new MobtimerRepository();
+    const members = repo.getMembers();
+    return members.includes(name);
+  }
 }
