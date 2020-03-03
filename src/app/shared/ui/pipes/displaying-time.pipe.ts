480683d235076d244dbb5fe87768b45b682a2bbc
Wataru.Kasahara <kasahara1019@gmail.com>
2020-03-04 08:47:42 +0900

    時間表示用に変換する pipe を実装

--- src/app/shared/ui/pipes/displaying-time.pipe.ts
+++ src/app/shared/ui/pipes/displaying-time.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const zeroPadding = (num: number): string => {
+  return num.toString().padStart(2, '0');
+};
+
 @Pipe({
   name: 'displayingTime',
 })
 export class DisplayingTimePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(time: { minutes: number; seconds: number }): string {
+    return `${zeroPadding(time.minutes)}:${zeroPadding(time.seconds)}`;
   }
-
 }
