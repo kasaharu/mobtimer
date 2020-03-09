648e75807b0ecacd106926e7bed6584833c47f66
Wataru.Kasahara <kasahara1019@gmail.com>
2020-03-09 21:39:22 +0900

    domain が使う repository の修正漏れ

--- src/app/domain/mobbing/mob-member.service.ts
+++ src/app/domain/mobbing/mob-member.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { MobtimerRepository } from '../../infrastructures/repositories/mobtimer.repository';
+import { MobMemberRepository } from '../../infrastructures/repositories/mob-member.repository';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +8,7 @@ export class MobMemberService {
   constructor() {}
 
   static exists(name: string) {
-    const repo = new MobtimerRepository();
+    const repo = new MobMemberRepository();
     const members = repo.getMembers();
     return members.map((member) => member.name).includes(name);
   }
