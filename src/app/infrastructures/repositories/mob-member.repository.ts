eef3dd59796d5cad2159ccc270cdef5637787e6d
Wataru.Kasahara <kasahara1019@gmail.com>
2020-03-10 21:33:10 +0900

    storagePrefix を helper として切り出し

--- src/app/infrastructures/repositories/mob-member.repository.ts
+++ src/app/infrastructures/repositories/mob-member.repository.ts
@@ -1,31 +1,32 @@
 import { Injectable } from '@angular/core';
 import { MobMemberProps } from '../../domain/mobbing/mob-member.vo';
+import { storagePrefix } from '../helpers/storage-prefix';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MobMemberRepository {
   constructor() {}
-  readonly storagePrefix = 'kasaharu/mobtimer';
+  readonly storageKey = `${storagePrefix}/members`;
 
   saveMember(member: MobMemberProps) {
-    const savedMembersString = localStorage.getItem(`${this.storagePrefix}/members`);
+    const savedMembersString = localStorage.getItem(this.storageKey);
     const savedMembers = savedMembersString ? JSON.parse(savedMembersString) : [];
     const newMembers = [member].concat(savedMembers);
 
-    localStorage.setItem(`${this.storagePrefix}/members`, JSON.stringify(newMembers));
+    localStorage.setItem(this.storageKey, JSON.stringify(newMembers));
   }
 
   deleteMember(targetMemberName: string) {
-    const savedMembersString = localStorage.getItem(`${this.storagePrefix}/members`);
+    const savedMembersString = localStorage.getItem(this.storageKey);
     const savedMembers: MobMemberProps[] = savedMembersString ? JSON.parse(savedMembersString) : [];
     const newMembers = savedMembers.filter((member) => member.name !== targetMemberName);
 
-    localStorage.setItem(`${this.storagePrefix}/members`, JSON.stringify(newMembers));
+    localStorage.setItem(this.storageKey, JSON.stringify(newMembers));
   }
 
   getMembers(): MobMemberProps[] {
-    const savedMembersString = localStorage.getItem(`${this.storagePrefix}/members`);
+    const savedMembersString = localStorage.getItem(this.storageKey);
     return savedMembersString ? (JSON.parse(savedMembersString) as MobMemberProps[]) : [];
   }
 }
--- src/app/infrastructures/repositories/mob-time.repository.ts
+++ src/app/infrastructures/repositories/mob-time.repository.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { MobTime, MobTimeProps } from '../../domain/mobbing/mob-time.vo';
+import { storagePrefix } from '../helpers/storage-prefix';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MobTimeRepository {
   constructor() {}
-  readonly storagePrefix = 'kasaharu/mobtimer';
+  readonly storageKey = `${storagePrefix}/minutes`;
 
   saveTime(minutes: MobTimeProps) {
-    localStorage.setItem(`${this.storagePrefix}/minutes`, JSON.stringify(minutes));
+    localStorage.setItem(this.storageKey, JSON.stringify(minutes));
   }
 
   getTime(): MobTimeProps {
-    const minutes = localStorage.getItem(`${this.storagePrefix}/minutes`);
+    const minutes = localStorage.getItem(this.storageKey);
     return minutes ? (JSON.parse(minutes) as MobTimeProps) : { count: MobTime.minimumTime };
   }
 }
