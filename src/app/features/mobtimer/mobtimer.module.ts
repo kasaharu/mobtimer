3c7df552de172cd58ed975a050361ad16b7d281f
Wataru.Kasahara <kasahara1019@gmail.com>
2020-03-29 16:02:28 +0900

    yarn ng g c features/mobtimer/ui/containers/timer

--- src/app/features/mobtimer/mobtimer.module.ts
+++ src/app/features/mobtimer/mobtimer.module.ts
@@ -5,12 +5,13 @@ import { StoreModule } from '@ngrx/store';
 import { SharedModule } from '../../shared/shared.module';
 import { default as reducer, featureName } from './store/index.store';
 import { SettingComponent } from './ui/containers/setting/setting.component';
+import { TimerComponent } from './ui/containers/timer/timer.component';
 import { MobtimerComponent } from './ui/pages/mobtimer/mobtimer.component';
 import { MembersComponent } from './ui/presenters/members/members.component';
 import { TimeComponent } from './ui/presenters/time/time.component';
 
 @NgModule({
-  declarations: [MobtimerComponent, TimeComponent, MembersComponent, SettingComponent],
+  declarations: [MobtimerComponent, TimeComponent, MembersComponent, SettingComponent, TimerComponent],
   imports: [CommonModule, ReactiveFormsModule, StoreModule.forFeature(featureName, reducer), SharedModule],
 })
 export class MobtimerModule {}
---
+++ src/app/features/mobtimer/ui/containers/timer/timer.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
