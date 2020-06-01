f32cfd15490e3ed9bdf8a3bdf43c2f224afad541
Wataru.Kasahara <kasahara1019@gmail.com>
2020-06-01 21:43:19 +0900

    Fix E2E spec

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('mobtimer app is running!');
+    expect(page.getTitleText()).toEqual('MobTimer');
   });
 
   afterEach(async () => {
